653c151e5cfeffe9028490abd08c6a75d84a6711
Jakub Supa <jakub.supa@icloud.com>
2021-02-09 22:38:58 +0100

    /check

--- index.js
+++ index.js
@@ -7,48 +7,48 @@ var http = require('http'),
 	handlers = require('./lib/handlers.js'),
 	helpers = require('./lib/helpers.js');
 
-var httpServer = http.createServer(function(req,res){
-	unifiedServer(req,res);
+var httpServer = http.createServer(function (req, res) {
+	unifiedServer(req, res);
 });
 
-httpServer.listen(config.httpPort, function(){
-	console.log('Server listening on port '+config.httpPort);
+httpServer.listen(config.httpPort, function () {
+	console.log('Server listening on port ' + config.httpPort);
 });
 
 var httpsServerOptions = {
-	'key' : fs.readFileSync('./https/key.pen'),
-	'cert' : fs.readFileSync('./https/cart.pen')
+	'key': fs.readFileSync('./https/key.pen'),
+	'cert': fs.readFileSync('./https/cart.pen')
 };
 
-var httpsServer = https.createServer(httpsServerOptions, function(req,res,){
-	unifiedServer(req,res);
+var httpsServer = https.createServer(httpsServerOptions, function (req, res,) {
+	unifiedServer(req, res);
 });
 
-httpsServer.listen(config.httpsPort, function(){
-	console.log('Server listening on port '+config.httpsPort);
+httpsServer.listen(config.httpsPort, function () {
+	console.log('Server listening on port ' + config.httpsPort);
 });
 
-var unifiedServer = function(req,res){
-	var ParsedUrl = url.parse(req.url,true),
+var unifiedServer = function (req, res) {
+	var ParsedUrl = url.parse(req.url, true),
 		Path = ParsedUrl.pathname,
-		TrimmedPath = Path.replace(/^\/+|\/+$/g,''),
+		TrimmedPath = Path.replace(/^\/+|\/+$/g, ''),
 		Method = req.method.toLowerCase(),
 		QueryStringObject = ParsedUrl.query,
 		Headers = req.headers,
 		Decoder = new stringDecoder('utf-8'),
 		buffer = '';
 
-	req.on('data',function(data){
+	req.on('data', function (data) {
 
 		buffer += Decoder.write(data);
 
 	});
 
-	req.on('end',function(){
+	req.on('end', function () {
 
 		buffer += Decoder.end();
 
-		var ChoseHandler = typeof(router[TrimmedPath]) != 'undefined' ? router[TrimmedPath] : handlers.notFound;
+		var ChoseHandler = typeof (router[TrimmedPath]) != 'undefined' ? router[TrimmedPath] : handlers.notFound;
 
 		var data = {
 			'TrimmedPath': TrimmedPath,
@@ -57,11 +57,11 @@ var unifiedServer = function(req,res){
 			'Headers': Headers,
 			'Payload': helpers.parseJsonToObject(buffer)
 		}
-		
-		ChoseHandler(data, function(statusCode, Payload){
 
-			statusCode = typeof(statusCode) == 'number' ? statusCode : 200;
-			Payload = typeof(Payload) == 'object' ? Payload : {'error':404,'message':'Not Found'};
+		ChoseHandler(data, function (statusCode, Payload) {
+
+			statusCode = typeof (statusCode) == 'number' ? statusCode : 200;
+			Payload = typeof (Payload) == 'object' ? Payload : { 'error': 404, 'message': 'Not Found' };
 			var PayloadString = JSON.stringify(Payload);
 
 			res.setHeader('Content-Type', 'application/json');
@@ -75,5 +75,6 @@ var unifiedServer = function(req,res){
 var router = {
 	'ping': handlers.ping,
 	'users': handlers.users,
-	'tokens': handlers.tokens
+	'tokens': handlers.tokens,
+	'check': handlers.check
 };
